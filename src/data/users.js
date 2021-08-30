f5268b90c9baaab545c214a1401270037b2f1bd9
DidierSoules <36740732+DidierSoules@users.noreply.github.com>
2021-08-30 10:21:55 -0300

    Created array of users.

---
+++ src/data/users.js
@@ -0,0 +1,18 @@
+const Users = [
+  {
+    user: "test",
+    pass: "test",
+    name: "Test",
+    enabled: false,
+    permits: ["support"]
+  },
+  {
+    user: "juapernu",
+    pass: "test",
+    name: "Juan Manuel",
+    enabled: true,
+    permits: ["support", "learning"]
+  },
+]
+
+export default Users;
